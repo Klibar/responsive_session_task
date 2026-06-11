688ac5baa7ab0d7512bf48b1e353b164861ca01b
Klibar <ma8964032@gmail.com>
2026-06-11 23:21:13 +0300

    first commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyColors {
   static Color mainColor = Color(0xff5dc9a5);
   static Color secondColor = Color(0xff075142);
+}
+
+class CategoriesBtn extends StatelessWidget {
+  const CategoriesBtn({super.key, required this.btnColor, required this.tilte});
+  final Color btnColor;
+  final String tilte;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: btnColor,
+        borderRadius: BorderRadius.circular(18.r),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Text(
+          tilte,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.sp,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -18,154 +18,266 @@ class _HomescreenState extends State<HomeScreen> {
         backgroundColor: Color(0xff30302e),
         body: Padding(
           padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: searchController,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                        hintText:
-                            ('Search destinations, cities, experiences ... '),
-                        hintStyle: TextStyle(
-                          color: Color(0xffb8b9b3),
-                          fontSize: 18.sp,
-                        ),
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Color(0xffb8b9b3),
-                          size: 30.sp,
-                        ),
-                        suffixIcon: Icon(
-                          Icons.menu_open_sharp,
-                          size: 30.sp,
-                          color: Color(0xffb8b9b3),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.r),
-                          borderSide: BorderSide(
-                            color: Color(0xFF444442),
-                            width: 4.w,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 200),
+                          hintText:
+                              ('Search destinations, cities, experiences ... '),
+                          hintStyle: TextStyle(
+                            color: Color(0xffb8b9b3),
+                            fontSize: 18.sp,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color(0xffb8b9b3),
+                            size: 30.sp,
+                          ),
+                          suffixIcon: Icon(
+                            Icons.menu_open_sharp,
+                            size: 30.sp,
+                            color: Color(0xffb8b9b3),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.r),
+                            borderSide: BorderSide(
+                              color: Color(0xFF444442),
+                              width: 4.w,
+                            ),
                           ),
                         ),
                       ),
                     ),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 200.h,
+                        decoration: BoxDecoration(
+                          color: MyColors.mainColor,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20.r),
+                            topRight: Radius.circular(20.r),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: Row(
+                            crossAxisAlignment: .end,
+                            children: [
+                              Container(
+                                width: 140.w,
+                                height: 35.h,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20.r),
+                                  ),
+                                  color: Color(0xff9de1ca),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: .center,
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: MyColors.secondColor,
+                                      size: 20,
+                                    ),
+                                    SizedBox(width: 3.w),
+                                    Text(
+                                      'Featured',
+                                      style: TextStyle(
+                                        color: MyColors.secondColor,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.sp,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 150.h,
+                        decoration: BoxDecoration(
+                          color: MyColors.secondColor,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20.r),
+                            bottomRight: Radius.circular(20.r),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25,
+                            vertical: 20,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            mainAxisAlignment: .spaceEvenly,
+                            children: [
+                              Text(
+                                'TOP DESTINATION',
+                                style: TextStyle(
+                                  color: MyColors.mainColor,
+                                  fontSize: 20.sp,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.1,
+                                ),
+                              ),
+                              Text(
+                                'Santorini, Greece',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 31.sp,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.1,
+                                ),
+                              ),
+                              Text(
+                                'Cliffside views & iconic sunsets',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontSize: 20.sp,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                Row(
+                  mainAxisAlignment: .spaceBetween,
+                  children: [
+                    Text(
+                      'Categories',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.sp,
+                        letterSpacing: -1,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                        color: MyColors.secondColor,
+                        letterSpacing: -1,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.h),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      CategoriesBtn(btnColor: MyColors.mainColor, tilte: 'All'),
+                      SizedBox(width: 20.w),
+                      CategoriesBtn(
+                        btnColor: Color(0xff262624),
+                        tilte: 'Beach',
+                      ),
+                      SizedBox(width: 20.w),
+                      CategoriesBtn(
+                        btnColor: Color(0xff262624),
+                        tilte: 'Mountain',
+                      ),
+                      SizedBox(width: 20.w),
+                      CategoriesBtn(btnColor: Color(0xff262624), tilte: 'City'),
+                      SizedBox(width: 20.w),
+                      CategoriesBtn(
+                        btnColor: Color(0xff262624),
+                        tilte: 'Culture',
+                      ),
+                      SizedBox(width: 20.w),
+                      CategoriesBtn(
+                        btnColor: Color(0xff262624),
+                        tilte: 'Nature',
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              SizedBox(height: 20.h),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 200.h,
+                ),
+                SizedBox(height: 10.h),
+                Row(
+                  mainAxisAlignment: .spaceBetween,
+                  children: [
+                    Text(
+                      'Popular',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.sp,
+                        letterSpacing: -1,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                        color: MyColors.secondColor,
+                        letterSpacing: -1,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.h),
+                Row(
+                  children: [
+                    Container(
+                      width: 110.w,
+                      height: 100.h,
                       decoration: BoxDecoration(
                         color: MyColors.mainColor,
                         borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20.r),
-                          topRight: Radius.circular(20.r),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(25),
-                        child: Row(
-                          crossAxisAlignment: .end,
-                          children: [
-                            Container(
-                              width: 140.w,
-                              height: 35.h,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(20.r),
-                                ),
-                                color: Color(0xff9de1ca),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: .center,
-                                children: [
-                                  Icon(
-                                    Icons.star,
-                                    color: MyColors.secondColor,
-                                    size: 20,
-                                  ),
-                                  SizedBox(width: 3.w),
-                                  Text(
-                                    'Featured',
-                                    style: TextStyle(
-                                      color: MyColors.secondColor,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.sp,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
+                          bottomLeft: Radius.circular(15.r),
+                          topLeft: Radius.circular(15.r),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 140.h,
-                      decoration: BoxDecoration(
-                        color: MyColors.secondColor,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(20.r),
-                          bottomRight: Radius.circular(20.r),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 25,
-                          vertical: 20,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: .start,
-                          mainAxisAlignment: .spaceEvenly,
-                          children: [
-                            Text(
-                              'TOP DESTINATION',
-                              style: TextStyle(
-                                color: MyColors.mainColor,
-                                fontSize: 20.sp,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 0.1,
-                              ),
-                            ),
-                            Text(
-                              'Santorini, Greece',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 31.sp,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 0.1,
-                              ),
-                            ),
-                            Text(
-                              'Cliffside views & iconic sunsets',
-                              style: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
-                                fontSize: 20.sp,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 0.1,
-                              ),
-                            ),
-                          ],
-                        ),
+                    Container(
+                      decoration: BoxDecoration(),
+                      child: Column(
+                        children: [
+                          Text('Kyoto'),
+                          Text('Japan'),
+                          Row(
+                            children: [
+                              Row(children: [Icon(Icons.star), Text('4.9')]),
+                              Text('\$820'),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
