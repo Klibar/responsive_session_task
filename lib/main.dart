2de98264258f40f822184aa72abefc5810d199d5
Klibar <ma8964032@gmail.com>
2026-06-17 19:29:10 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Responsive Task App',
+          title: 'Responcive Task App',
           home: const ResponsiveLayout(),
         );
       },
